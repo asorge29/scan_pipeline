7337ec8b8f1e12b0392dcdb0ffbbf1260eefeb03
Andy Sorge <124544413+asorge29@users.noreply.github.com>
2024-04-30 17:59:23 -0700

    enable scaling for high dpi

--- scan_pipeline/app.py
+++ scan_pipeline/app.py
@@ -1,12 +1,9 @@
-import sys
-from PyQt5.QtWidgets import QApplication
-from .views import Window
-
-import sys
+import sys, os
 from PyQt5.QtWidgets import QApplication
 from .views import Window
 
 def main():
+    os.environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
     app = QApplication(sys.argv)
     win = Window()
     win.show()
