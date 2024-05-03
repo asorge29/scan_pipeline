a65544f10b3142b5d7c8fedd7f08a563385f30c8
asorge29 <124544413+asorge29@users.noreply.github.com>
2024-05-02 19:47:34 -0700

    added requirements file

--- scan_pipeline/views.py
+++ scan_pipeline/views.py
@@ -42,6 +42,9 @@ class Window(QWidget, Ui_Form):
         self.changeOutputButton.clicked.connect(self._chooseOutputFolder)
         self.renameButton.clicked.connect(self._renameFile)
         self.newName.returnPressed.connect(self._renameFile)
+        self.deleteButton.clicked.connect(self._deleteFile)
+        self.rotateButton.clicked.connect(self._rotateImage)
+        self.reloadButton.clicked.connect(self._reloadImage)
 
     def _startFileWatcher(self):
         if self._inputFolder:
@@ -57,6 +60,9 @@ class Window(QWidget, Ui_Form):
 
     def _chooseOutputFolder(self):
         self._outputFolder = Path(QFileDialog.getExistingDirectory(self, "Select output folder"))
+        self.outputDirLabel.setText(f'Output Directory: {self._outputFolder}')
+        if self._outputFolder == Path("."):
+            self._outputFolder = None
         self.outputDirLabel.setText(f'Output Directory: {self._outputFolder}')
 
     def _checkImage(self, path:Path):
@@ -82,8 +88,8 @@ class Window(QWidget, Ui_Form):
             self._stateFilesLoaded()
             self._currentFile = self._queue.pop(0)
             newImage = QPixmap(str(self._currentFile))
-            w, h = self.imageDisplay.width(), self.imageDisplay.height()
-            self.imageDisplay.setPixmap(newImage.scaled(w, h, QtCore.Qt.AspectRatioMode.KeepAspectRatio))
+            w, h = self.imageLabel.width(), self.imageLabel.height()
+            self.imageLabel.setPixmap(newImage.scaled(w, h, QtCore.Qt.AspectRatioMode.KeepAspectRatio))
 
     def _renameFile(self):
         if self._outputFolder is not None:
@@ -106,10 +112,15 @@ class Window(QWidget, Ui_Form):
         self.imageLabel.setText('No more files in input folder.')
         self._currentFile = None
         self.newName.setEnabled(False)
-        
+        self.deleteButton.setEnabled(False)
+        self.rotateButton.setEnabled(False)
+        self.reloadButton.setEnabled(False)
     
     def _stateFilesLoaded(self):
         self.newName.setEnabled(True)
+        self.deleteButton.setEnabled(True)
+        self.rotateButton.setEnabled(True)
+        self.reloadButton.setEnabled(True)
 
     def _validatePrefixChars(self):
         #check for characters that are not allowed in file names
@@ -131,3 +142,18 @@ class Window(QWidget, Ui_Form):
         msg.setWindowTitle(title)
         msg.setText(text)
         msg.exec_()
+
+    def _deleteFile(self):
+        if self._currentFile != None:
+            self._currentFile.unlink()
+            self._nextFile()
+
+    def _rotateImage(self):
+        rotated = Image.open(self._currentFile).rotate(90, expand=True)
+        rotated.save(self._currentFile)
+        self._reloadImage()
+
+    def _reloadImage(self):
+        updatedImage = QPixmap(str(self._currentFile))
+        w, h = self.imageLabel.width(), self.imageLabel.height()
+        self.imageLabel.setPixmap(updatedImage.scaled(w, h, QtCore.Qt.AspectRatioMode.KeepAspectRatio))
